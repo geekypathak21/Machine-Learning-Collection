5dd8a552321b1194965f8f262d24d5117d719964
himanshupathak21061998 <hpathak336@gmail.com>
2018-08-09 02:56:41 -0400

    It is my machine Learning Programs

---
+++ Regression-models/First-Linear_model/First-Machine-Learning-Model.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  3 01:19:13 2018
+
+@author: Himanshu
+"""
+# Data Preprocessing Template
+
+# Importing the libraries
+
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+#setting the linear on x_train and y_train
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor.fit( X_train ,y_train)
+
+y_pred=regressor.predict(X_train)
+plt.scatter(X_train,y_train,color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('Salary vs Expirience')
+plt.xlabel('years of Experience')
+plt.ylabel('Salary')
+plt.show()
+plt.scatter(X_test,y_test,color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('Salary vs Expirience')
+plt.xlabel('years of Experience')
+plt.ylabel('Salary')
+plt.show()
